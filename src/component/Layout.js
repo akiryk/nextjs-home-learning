a6356bb2aec2c3179da075ac5ad6e30defd67477
adam kiryk <akiryk@wayfair.com>
2020-12-15 16:33:17 -0500

    navigation changes

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -1,23 +1,20 @@
 import NavBar from './Navbar';
 import Head from 'next/head';
+import styles from './Layout.module.scss';
 
 const layout = ({ children }) => {
   return (
     <>
       <Head>
+        <title>Wayfair Learning Academy</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
           rel="stylesheet"
-        ></link>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
         />
-        <link rel="stylesheet" href="/style.css" />
       </Head>
       <NavBar />
-      {children}
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <div className={styles.wrapper}>{children}</div>
     </>
   );
 };
